413e192e5e4e93759887437976dc3cc0a2a1d213
sarogoki <shanmugamgoki@gmail.com>
2016-08-06 18:10:40 +0530

    Add files via upload

---
+++ lex.java
@@ -0,0 +1,10 @@
+class CompareToExample
+{
+    public static void main(String args[])
+    {
+    String st="abcde";
+    String st1="abcdf";
+   
+    System.out.println(st.compareTo(st1));
+    }
+}
