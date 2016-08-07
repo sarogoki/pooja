9707f4349ec710bb5d650dbe1b453d6ca29c56e1
sarogoki <shanmugamgoki@gmail.com>
2016-08-07 11:14:24 +0530

    Add files via upload

--- reverse.java
+++ reverse.java
@@ -1,19 +1,40 @@
-import java.io.*;
-import java.util.*;
-
-public class reverseString {
-    public static void main(String[] args) {
-        String input="";
-        System.out.println("Enter the input string");
-        try
-        {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            input = br.readLine();
-            char[] try1= input.toCharArray();
-            for (int i=try1.length-1;i>=0;i--)
-            System.out.print(try1[i]);
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-}}
+public class ReverseEachWord
+{
+	static void reverseEachWordOfString(String inputString)
+	{
+		String[] words = inputString.split(" ");
+		
+		String reverseString = "";
+		
+		for (int i = 0; i < words.length; i++) 
+		{
+			String word = words[i];
+			
+			String reverseWord = "";
+			
+			for (int j = word.length()-1; j >= 0; j--) 
+			{
+				reverseWord = reverseWord + word.charAt(j);
+			}
+			
+			reverseString = reverseString + reverseWord + " ";
+		}
+		
+		System.out.println(inputString);
+		
+		System.out.println(reverseString);
+		
+		System.out.println("-------------------------");
+	}
+	
+    public static void main(String[] args) 
+    {
+		reverseEachWordOfString("Java Concept Of The Day");
+		
+		reverseEachWordOfString("Java J2EE JSP Servlets Hibernate Struts");
+		
+		reverseEachWordOfString("I am string not reversed");
+		
+		reverseEachWordOfString("Reverse Me");
+	}
+}
